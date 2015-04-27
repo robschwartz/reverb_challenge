b97fefab411e37f90d212a45a5b5cde650320f6b
robschwartz <robertschwar@gmail.com>
2015-04-26 22:41:11 -0500

    started combiner programer

--- filereader.rb
+++ filereader.rb
@@ -1,15 +1,29 @@
-class People
-	def initialize
-		@people = []
+class Compiler
+
+	def self.combine_files(*files)
+		people = []
+		files.each do |file|
+			File.open(file, 'r') do |f|
+				f.each do |line|
+					File.open("full_list.txt", "a") << line.gsub("|", " ").gsub(",", " ") 
+				end
+			end
+		end
 	end
 
-	file = ARGV.first
-	puts "here's the input i got:"
-	File.open(file, "r") do |f|
-		f.each_line do |line|
-			line = line.gsub("|", " ").gsub(",", " ")
-			@people << line.split[2]
-	  end
-	end
-	puts @people
+	# def self.read_file(file)
+	# 	people = []
+	# 	File.open(file, "r") do |f|
+	# 		f.each_line do |line|
+	# 			line = line.gsub("|", " ").gsub(",", " ")
+	# 			people << line
+	# 		end
+	# 	end
+	# 	people
+	# end
 end
+
+Compiler.combine_files("space_names.txt", "pipe_names.txt", "comma_names.txt")
+# file = ARGV.first
+# puts "here's the input i got:"
+# p People.read_file(file)
